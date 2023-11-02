9ba7fb364e5e89f0705a5256345c27dccc6e08d3
Taras Iskiv <taras.iskiv@gmail.com>
2023-11-02 21:58:40 +0100

    Added task controller

---
+++ TaskService/TaskManager.Task.API/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Core.Payloads;
+using TaskManager.Task.Logic.Abstraction;
+
+namespace TaskManager.Task.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TaskController : ControllerBase
+{
+    private readonly ITaskService _taskService;
+
+    public TaskController(ITaskService taskService)
+    {
+        _taskService = taskService;
+    }
+
+    [HttpPost("CreateTask")]
+    public async Task<IActionResult> CreateTask(CreateTaskPayload payload)
+    {
+        await _taskService.CreateTask(payload);
+        return Ok();
+    }
+
+    [HttpGet("GetTask")]
+    public async Task<IActionResult> GetTask([FromQuery] int taskId)
+    {
+        var task = await _taskService.GetTask(taskId);
+        return Ok(task);
+    }
+    
+    [HttpGet("GetTasks")]
+    public async Task<IActionResult> GetTasks()
+    {
+        var tasks = await _taskService.GetTasks();
+        return Ok(tasks);
+    }
+
+}
